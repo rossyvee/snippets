687b0c521b8ca6408964592ee0be1cb022118a7e
rossyvee <roseakinyi001@gmail.com>
2022-06-16 11:28:04 +0300

    adding updates

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,5 +6,4 @@ class SnippetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style', )
+        fields = ('id', 'title', 'code', 'linenos','language', 'style', )
